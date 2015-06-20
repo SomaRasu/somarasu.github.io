c6b018d449de2a508f6568df9e6c3edc8823618e
SomaRasu <mhaynes1337@gmail.com>
2015-06-20 09:15:17 -0700

    ho
    ho

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-console.log("yo, imma worker");
-
 this.addEventListener('fetch', function(event) {
-    event.respondWith(new Response("What's good, world!"));
+    console.log("yo, imma worker");
 });
