91071e54707f9b9dd048b72dff7280f9ad8a959c
SomaRasu <mhaynes1337@gmail.com>
2015-06-27 17:25:43 -0700

    css tweak
    css tweak

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
 importScripts('/cache-polyfill-master/index.js');
-self.addEventListener('fetch', function(event) {
-    console.log("yo, imma worker");
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/scripts/jquery-ui.min.js',
+  '/scripts/jquery-1.11.3.js'
+];
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache){
+           console.log("got dat cache open, son.");
+           return cache.addAll(urlsToCache);
+        })
+    );
 });
