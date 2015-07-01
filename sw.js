e12ea55983b6e23701ecfd8b95310d005d986800
SomaRasu <mhaynes1337@gmail.com>
2015-06-30 22:32:11 -0700

    a change
    a change

--- sw.js
+++ sw.js
@@ -25,7 +25,6 @@ self.addEventListener('fetch', function(event) {
                     return response;
                 }
                 return fetch(event.request);
-
             }
         )
     );
