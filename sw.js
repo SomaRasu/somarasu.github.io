a48a657d4983c53b4f0da213adabb4644026cb73
SomaRasu <mhaynes1337@gmail.com>
2015-06-30 22:14:52 -0700

    yo
    tyo

--- sw.js
+++ sw.js
@@ -1,19 +1,32 @@
 importScripts('/cache-polyfill-master/index.js');
 
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/',
-  '/styles/main.css',
-  '/scripts/jquery-ui.min.js',
-  '/scripts/jquery-1.11.3.js',
-  '/scripts/botchamania.js'
-];
+var CACHE_NAME = 'my-site-cache-v1',
+    urlsToCache = [
+        '/',
+        '/styles/main.css',
+        '/scripts/jquery-ui.min.js',
+        '/scripts/jquery-1.11.3.js'
+    ];
 
 self.addEventListener('install', function(event){
     event.waitUntil(
         caches.open(CACHE_NAME).then(function(cache){
-           console.log("got dat cache open, son.");
-           return cache.addAll(urlsToCache);
+            console.log("got dat cache open, son! ayyy");
+            return cache.addAll(urlsToCache);
         })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+
+            }
+        )
     );
 });
