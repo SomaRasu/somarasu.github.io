dddde28091a0ca9c40282077d6a83bb795d006bb
somarasu <mhaynes1337@gmail.com>
2016-11-30 12:36:18 -0800

    cold like Minnesota
    cold like Minnesota

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
+// If 'navigation.serviceworker' is available, go 'head and wind it up!
+// ( or try to )
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js', {
-        scope: '/'
+        scope: '/' // scope == origin, so it'll be registered wherever you go
     }).then(function(reg) {
-        console.log('Yoooooo!', reg);
+        console.log('Yoooooo!', reg); // it worked! :D
     }).catch(function(err) {
-        console.log('Fuuuuuck!', err);
+        console.log('Fuuuuuck!', err); // it didnt work D:
     });
 }
 
